7d5d81589aa3791b3bd375f2a7aa3d3541b100fe
Raven Ironwing <ravenironwing@gmail.com>
2021-08-28 01:41:15 -0700

    Add files via upload

--- Novris.py
+++ Novris.py
@@ -1,6 +1,6 @@
 # Novris by Raven Ironwing
-# A simple Tetris like game based on blocks that fit in a 3x3 grid up to 5 cells.
-# I used Tetris by "Kevin Chabowski"<kevin@kch42.de> as a starting point for this game.
+# A simple novris like game based on blocks that fit in a 3x3 grid up to 5 cells.
+# I used novris by "Kevin Chabowski"<kevin@kch42.de> as a starting point for this game.
 
 # Music attributions
 # music0.ogg cuts from NES-Bach-BMV-565, TheOuterLinux, https://opengameart.org/content/nes-bach-bwv-565
@@ -11,11 +11,10 @@
 # music5 Cyberpunk Moonlight Sonata by Joth, https://opengameart.org/content/cyberpunk-moonlight-sonata
 # music6 C64 - Dies Irae, TheOuterLinux, https://opengameart.org/content/c64-mozart-dies-irae
 # music7.ogg Sonata 8, Kim Lightyear, Bernd Krueger https://opengameart.org/content/sonata-8-chiptune-beethoven
-# music8.ogg "Twister Tetris" by poinl, https://opengameart.org/content/twister-tetris
+# music8.ogg "Twister novris" by poinl, https://opengameart.org/content/twister-novris
 import random, sys, os, copy
 import pygame as pg
-import numpy as np
-from random import randrange as rand
+from random import choice, choices
 from os import path, listdir
 
 main_folder = path.dirname(__file__) #comment out when using pyinstaller hates this when packaging into an executable for some reason.
@@ -47,7 +46,6 @@ INDIGO = (75, 0, 130)
 VIOLET = (163, 0, 255)
 CYAN = (0, 255, 255)
 MAGENTA = (255, 0, 255)
-LIGHTBLUE = (0, 155, 155)
 BROWN = (165,42,42)
 
 FONT_NAME = 'arial'
@@ -61,7 +59,6 @@ GREEN,
 BLUE,
 ORANGE,
 VIOLET,
-LIGHTBLUE,
 MAGENTA,
 INDIGO,
 CYAN,
@@ -70,7 +67,7 @@ BROWN
 
 
 # Define the shapes of the single parts
-tetris_shapes = [
+novris_shapes = [
 	[[1]],
 
 	[[0, 0, 0],
@@ -130,9 +127,82 @@ tetris_shapes = [
 
 	[[0, 1, 0],
 	 [1, 1, 1],
-	 [1, 0, 0]]
+	 [1, 0, 0]],
+
+	[[1, 1, 1],
+	 [1, 0, 1],
+	 [1, 0, 0]],
+
+	[[1, 1, 1],
+	 [1, 1, 0],
+	 [1, 0, 0]],
+
+	[[0, 1, 1],
+	 [1, 1, 1],
+	 [1, 0, 0]],
+
+	[[0, 1, 0],
+	 [1, 1, 1],
+	 [1, 1, 0]],
+
+	[[1, 0, 1],
+	 [1, 1, 1],
+	 [1, 0, 0]],
+
+	[[1, 1, 0],
+	 [1, 1, 1],
+	 [1, 0, 0]],
+
+	[[1, 1, 1],
+	 [1, 1, 1],
+	 [0, 0, 0]],
+
+	[[1, 0, 1],
+	 [1, 1, 1],
+	 [0, 1, 0]],
+
+	[[1, 0, 1],
+	 [1, 1, 1],
+	 [1, 0, 1]],
+
+	[[1, 0, 1],
+	 [1, 1, 1],
+	 [1, 1, 0]],
+
+	[[1, 0, 1],
+	 [1, 0, 1],
+	 [1, 1, 1]],
+
+	[[1, 1, 1],
+	 [1, 1, 0],
+	 [1, 1, 0]],
+
+	[[1, 1, 1],
+	 [1, 0, 1],
+	 [1, 1, 0]],
+
+	[[1, 1, 1],
+	 [1, 1, 1],
+	 [0, 1, 0]],
+
+	[[1, 1, 1],
+	 [1, 1, 1],
+	 [1, 1, 0]],
+
+	[[1, 1, 1],
+	 [1, 1, 0],
+	 [1, 1, 1]],
+
+	[[1, 1, 1],
+	 [1, 0, 1],
+	 [1, 1, 1]],
+
+	[[1, 1, 1],
+	 [1, 1, 1],
+	 [1, 1, 1]]
 ]
 
+SHAPE_PROB = [60, 60, 60, 60, 60, 60, 60, 60, 40, 40, 40, 40, 40, 40, 40, 40, 2, 2, 2, 2, 2, 2, 2, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
 novris_shape = [
 	 [1, 1, 0, 0, 0, 0, 0, 3, 0, 3, 0, 4, 4, 0, 5, 0, 0, 6, 6],
 	 [1, 0, 1, 0, 2, 2, 0, 3, 0, 3, 0, 4, 0, 0, 5, 0, 0, 6, 0],
@@ -174,6 +244,17 @@ def check_collision(board, boarder, shape, offset):
 				return True
 	return False
 
+def check_collision_side(board, boarder, shape, offset): # Used to allow the stone to be transformed when near edge.
+	off_x, off_y = offset
+	for cy, row in enumerate(shape):
+		for cx, cell in enumerate(row):
+			try:
+				if (cell and board[ cy + off_y][ cx + off_x]) or (cell and boarder[ cy + off_y + 1][ cx + off_x + 1]):
+					return (cx, cy)
+			except IndexError:
+				return (cx, cy)
+	return False
+
 def remove_row(board, row):
 	del board[row]
 	return [[0 for i in range(config['cols'])]] + board
@@ -188,16 +269,37 @@ def join_matrixes(mat1, mat2, mat2_off):
 				pass
 	return mat1
 
-def remove_color_row(board, row, col, color):
-	board = np.array(board)
+def remove_color_row(board, y, x):
+	lastx = 0
+	color = board[y][x]
+	for i in range(x, len(board[0])):
+		try:
+			if board[y][i] == color:
+				lastx = i
+			else:
+				break
+		except:
+			pass
+
+	for j in range(y, -1, -1):
+		for i in range(x, lastx + 1):
+			if j > 0:
+				board[j][i] = board[j - 1][i]
+			elif j == 0:
+				board[j][i] = 0
+	return board
+
+"""
+def old_remove_color_row(board, y, x, color):
+	board = np.array(board) # Makes an array to store the cells that were deleted.
 	deleted_cells = np.zeros_like(board)
-	board[row, col] = board[row, col + 1] = board[row, col + 2] = board[row, col + 3] = 0
-	deleted_cells[row, col] = deleted_cells[row, col + 1] = deleted_cells[row, col + 2] = deleted_cells[row, col + 3] = 1
+	board[y, x] = board[y, x + 1] = board[y, x + 2] = board[y, x + 3] = 0
+	deleted_cells[y, x] = deleted_cells[y, x + 1] = deleted_cells[y, x + 2] = deleted_cells[y, x + 3] = 1
 	for i in range(4, len(board[0])):
 		try:
-			if board[row, col + i] == color:
-				board[row, col + i] = 0
-				deleted_cells[row, col + i] = 1
+			if board[y, x + i] == color:
+				board[y, x + i] = 0
+				deleted_cells[y, x + i] = 1
 			else:
 				break
 		except:
@@ -206,15 +308,17 @@ def remove_color_row(board, row, col, color):
 	board = board.tolist()
 	return board
 
-def apply_gravity(board, deleted_cells):
+def apply_gravity(board, deleted_cells): # Splits the board into sub arrays to remove lines only below deleted cells.
 	cols_with_deletes = np.where(deleted_cells.any(axis=0))[0]
 	sub_arrays = np.split(board, (cols_with_deletes[0], cols_with_deletes[-1] + 1), 1)
+	dc_sub_arrays = np.split(deleted_cells, (cols_with_deletes[0], cols_with_deletes[-1] + 1), 1)
 	for num, arr in enumerate(sub_arrays):
-		for row in range(0, len(arr)):
-			for col in range(0, len(arr[0])):
-				if row > 0:
-					if arr[row - 1].any() and not arr[row].any():
-						sub_arrays[num] = remove_part_row(arr, row)
+		if dc_sub_arrays[num].any(): # Only drops the columns with deleted cells.
+			for row in range(0, len(arr)):
+				for col in range(0, len(arr[0])):
+					if row > 0 and deleted_cells[row].any: # Only drops rows with deleted cells.
+						if arr[row - 1].any() and not arr[row].any():
+							sub_arrays[num] = remove_part_row(arr, row)
 	board = np.concatenate(sub_arrays, 1)
 	return board
 
@@ -222,6 +326,7 @@ def remove_part_row(arr, row):
 	arr = np.delete(arr, row, axis=0)
 	new_row = np.zeros_like(arr[0])
 	return np.vstack([new_row, arr])
+"""
 
 def new_board():
 	board = [ [ 0 for x in range(config['cols']) ]
@@ -235,6 +340,8 @@ class NovrisApp(object):
 		pg.init()
 		pg.mixer.init()
 		pg.key.set_repeat(250,25)
+		self.channel3 = pg.mixer.Channel(2)
+		self.channel4 = pg.mixer.Channel(3)
 		self.font_name = pg.font.match_font(FONT_NAME)  # finds the closest match on computer
 		self.width = config['cell_size']*config['cols']
 		self.height = config['cell_size']*config['rows']
@@ -312,7 +419,7 @@ class NovrisApp(object):
 			self.level += 1
 			self.board = new_board()
 			pg.mixer.music.stop()
-			self.effects_sounds['levelup'].play()
+			self.channel4.play(self.effects_sounds['levelup'])
 			self.delay -= 30
 			if self.delay < 100:
 				self.delay = 100
@@ -329,16 +436,16 @@ class NovrisApp(object):
 
 	def new_stone(self):
 		global COLORS
-		if len(COLORS) <= self.level:
+		num_colors = self.level // 3 + 3
+		if num_colors > len(COLORS):
 			num_colors = len(COLORS)
-		else:
-			num_colors = self.level // 2 + 3
 		random_color = random.randrange(1, num_colors)
 
 		if self.next_stone != None:
 			self.stone = self.next_stone
 		else:
-			selected_shape = copy.deepcopy(tetris_shapes[rand(len(tetris_shapes))])
+			selected_shape = copy.deepcopy(choice(choices(novris_shapes, SHAPE_PROB, k=10)))
+			#selected_shape = copy.deepcopy(novris_shapes[random.randrange(len(novris_shapes))])
 			for j, row in enumerate(selected_shape):
 				for i, val in enumerate(row):
 					if val != 0:
@@ -350,7 +457,8 @@ class NovrisApp(object):
 		else:
 			self.stone_y = -1
 
-		next_selected_shape = copy.deepcopy(tetris_shapes[rand(len(tetris_shapes))])
+		next_selected_shape = copy.deepcopy(choice(choices(novris_shapes, SHAPE_PROB, k=10)))
+		#next_selected_shape = copy.deepcopy(novris_shapes[random.randrange(len(novris_shapes))])
 		for j, row in enumerate(next_selected_shape):
 			for i, val in enumerate(row):
 				if val != 0:
@@ -372,16 +480,17 @@ class NovrisApp(object):
 		self.new_stone()
 
 	def create_boarder(self):
-		boarder = [] # creates a matrix of zeros the size of the board surrounded but padded with 1s to use for collision detection.
+		boarder = [] # creates a matrix of zeros the size of the board surrounded but padded with 1s except for on top to use for collision detection.
 		for j in range(0, len(self.board) + 2):
 			new_row = []
 			for i in range(0, len(self.board[0]) + 2):
-				if (j in [0, len(self.board)+1]) or (i in [0, len(self.board[0])+1]):
+				if (j == len(self.board)+1) or (i in [0, len(self.board[0])+1]):
 					val = 1
 				else:
 					val = 0
 				new_row.append(val)
 			boarder.append(new_row)
+		print(boarder)
 		return boarder
 	
 	def center_msg(self, msg):
@@ -561,12 +670,12 @@ class NovrisApp(object):
 				self.stone_y += 1
 		self.stone_y -= 1
 		self.drop
-	
-	def drop(self):
+
+	def drop(self, insta_set = False):
 		if not self.gameover and not self.paused:
 			self.stone_y += 1
-			if check_collision(self.board, self.boarder, self.stone, (self.stone_x, self.stone_y)):
-				self.effects_sounds['set'].play()
+			if check_collision(self.board, self.boarder, self.stone, (self.stone_x, self.stone_y)) or insta_set:
+				self.channel3.play(self.effects_sounds['set'])
 				self.board = join_matrixes(self.board, self.stone, (self.stone_x, self.stone_y))
 				self.new_stone()
 				prevy = 0
@@ -596,40 +705,40 @@ class NovrisApp(object):
 										color_matches += 4 + self.countX(row, row[j], j+4) #Counts number of times matched colors appear in row
 										self.color_lines +=1
 										color_rows_removed += 1
-										self.board = remove_color_row(self.board, i, j, self.board[i][j])
+										self.board = remove_color_row(self.board, i, j)
 										break
 					else:
 						break
 
 				if rows_removed == 1:
 					self.score += 40 * (self.level + 1)
-					self.effects_sounds['line'].play()
+					self.channel4.play(self.effects_sounds['line'])
 				elif rows_removed == 2:
 					self.score += 100 * (self.level + 1)
-					self.effects_sounds['double'].play()
+					self.channel4.play(self.effects_sounds['double'])
 					self.lines += 1
 				elif rows_removed > 2:
 					if color_rows_removed == 3:
 						self.score += 2000 * (self.level + 1)
-						self.effects_sounds['novris'].play()
+						self.channel4.play(self.effects_sounds['novris'])
 						self.lines += 4
 						self.color_lines += 4
 						self.novris_animation(prevy)
 					else:
 						self.score += 300 * (self.level + 1)
-						self.effects_sounds['tripple'].play()
+						self.channel4.play(self.effects_sounds['tripple'])
 						self.lines += 2
 
 				if color_matches:
 					if color_matches < 7:
 						self.score += 20 * (self.level + 1)
-						self.effects_sounds['line'].play()
+						self.channel4.play(self.effects_sounds['line'])
 					elif color_matches < config['cols']:
 						self.score += 60 * (self.level + 1)
-						self.effects_sounds['double'].play()
+						self.channel4.play(self.effects_sounds['double'])
 					elif color_matches == config['cols']:
 						self.score += 200 * (self.level + 1)
-						self.effects_sounds['tripple'].play()
+						self.channel4.play(self.effects_sounds['tripple'])
 
 				self.lines_left = self.lines_required - self.lines
 				if self.lines_left < 0:
@@ -642,7 +751,7 @@ class NovrisApp(object):
 
 	def rotate_stone(self, orientation):
 		if not self.gameover and not self.paused:
-			self.effects_sounds['rotate'].play()
+			self.channel3.play(self.effects_sounds['rotate'])
 			if orientation == 1:
 				new_stone = rotate_counterclockwise(self.stone)
 			elif orientation == 2:
@@ -653,12 +762,26 @@ class NovrisApp(object):
 				new_stone = rotate_clockwise(self.stone)
 			if not check_collision(self.board, self.boarder, new_stone, (self.stone_x, self.stone_y)):
 				self.stone = new_stone
+			else:
+				col_point = check_collision_side(self.board, self.boarder, new_stone, (self.stone_x, self.stone_y))
+				if col_point[0] == 2:
+					if not check_collision(self.board, self.boarder, new_stone, (self.stone_x - 1, self.stone_y)):
+						self.stone = new_stone
+						self.move(-1)
+				if col_point[0] == 0:
+					if not check_collision(self.board, self.boarder, new_stone, (self.stone_x + 1, self.stone_y)):
+						self.stone = new_stone
+						self.move(1)
 
 	def toggle_pause(self):
 		now = pg.time.get_ticks()
 		if now - self.start_time > 500:
 			if not self.gameover:
 				self.paused = not self.paused
+		if self.paused:
+			pg.mixer.music.pause()
+		else:
+			pg.mixer.music.unpause()
 	
 	def start_game(self):
 		if self.gameover:
@@ -701,7 +824,8 @@ class NovrisApp(object):
 			'a': 		lambda:self.rotate_stone(1),
 			'd': 		lambda:self.rotate_stone(0),
 			'w':		lambda:self.rotate_stone(2),
-			's': 		lambda:self.rotate_stone(3)
+			's': 		lambda:self.rotate_stone(3),
+			'x':		lambda:self.drop(True)
 		}
 
 		pg.joystick.init() # Initializes all joysticks/controllers
@@ -759,6 +883,8 @@ class NovrisApp(object):
 						self.rotate_stone(1)
 					elif event.button == 3:
 						self.rotate_stone(3)
+					elif event.button == 4:
+						self.drop(True)
 					elif event.button == 5:
 						self.insta_fall()
 					elif event.button in [7, 9]:
